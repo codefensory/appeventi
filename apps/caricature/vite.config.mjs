fef61b1306a5d0a1a86f19c5e093ef65d2bccd4c
Yeferson <yefer.el.teke@gmail.com>
2025-07-05 10:08:10 -0500

    feat: remove federation

--- apps/caricature/vite.config.mjs
+++ apps/caricature/vite.config.mjs
@@ -37,7 +37,7 @@ export default defineConfig(({ command, mode }) => {
       rollupOptions: {
         external: [],
         output: {
-          format: 'system',
+          format: 'es',
           entryFileNames: 'assets/[name].js',
           minifyInternalExports: false
         }
